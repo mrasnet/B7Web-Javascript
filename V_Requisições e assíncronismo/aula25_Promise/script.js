c60f3f7278195a7c3d2c2d3e82dec88a08594736
Matheus Souza <contato.mralvessouza@gmail.com>
2021-08-25 15:47:49 -0300

    Inserção da aula 7 do modulo 5

--- V_Requisições e assíncronismo/aula25_Promise/script.js
+++ V_Requisições e assíncronismo/aula25_Promise/script.js
@@ -1,20 +1,21 @@
-/*---------------------( M3: Aula #25: Promise  )------------------------------------------------------------
-   Se utiliza quando se precisa de uma "promeça" - mensagem de garantia de que tal processo (que apresenta demora na resposta) vai retornar algo.
+/*---------------------( M3: Aula #25: Promise )------------------------------------------------------------
+    
+    Se utiliza quando se precisa de uma "promessa" - mensagem de garantia de que tal processo (que apresenta demora na resposta) vai retornar algo.
 
-   Ambos são definidos dentro da Promise:
+    Ambos são definidos dentro da Promise:
 
-   resolve      - usado para mandar a mensagem em si
-   reject       - usado para mandar mensagem de erro
+    resolve      - usado para mandar a mensagem em si
+    reject       - usado para mandar mensagem de erro
 
-   Ainda que não venhamos a criar nossas próprias promises, iremos utilizá-las, pois há muitas funções que as utilizam 
+    Ainda que não venhamos a criar nossas próprias promises, iremos utilizá-las, pois há muitas funções que as utilizam 
 */
 function fazer(){
-    //dentro do promise é necessário mandar uma função com os parametros: resolve / reject - ambas são funções
+    //dentro da promise é necessário mandar uma função com os parametros: resolve / reject - ambas são funções
     let promise = new Promise((resolve, reject) => {
         
         setTimeout(()=>{//setTimeout para simular uma espera
 
-            //outros comandos**** requisição a banco de dados, webservices etc.
+            //ex.: outros comandos**** requisição a banco de dados, webservices etc.
             resolve('OK'); //resposta 
             //Por enquanto não foi utilizado o reject
         }, 3000);
@@ -23,10 +24,10 @@ function fazer(){
     return promise; //ou, return new Promise
 }
 
-//Quem executar a função 'fazer' saberá imediatamente que vai receber uma promeça - que em determinado momento receberá um resposta dessa promeça
-//Ordem: executou o 'fazer', vai esperar uma resposta e quando a tiver vai fazer algo definido com ela na função que vai dentro do then
+//Quem executar a função 'fazer' saberá imediatamente que vai receber uma promessa - que em determinado momento receberá um resposta dessa 
+//Ordem: executou o 'fazer', vai esperar uma resposta e quando a tiver vai fazer algo definido com ela na função que vai dentro do 'then'
 //alguns programadores organizam da seguinte forma a chamada
 fazer()
     .then((resposta)=>{//Recebe a resposta quando disponível
         console.log("Resposta: "+resposta);//Utiliza essa resposta junto de comandos dentro da função
-    });
+});
---
+++ V_Requisições e assíncronismo/aula7_Promises/script.js
@@ -0,0 +1,59 @@
+/* --------------------- #M5 aula #7: Promises ----------------------- 
+
+    Quando realizamos uma requisição assíncrona ou um processo assíncrono a gente fica com alguma 'promessa' de resultado
+    
+    O promise basicamente é uma resultado temporário que recebemos - ou uma promessa - quando recebemos um objeto Promise, recebemos uma 
+    promessa de que daqui a algum tempo: - teremos três opções**
+
+    1. Nunca teremos o resultado - é uma promessa, portanto pode ou não se cumprir
+    2. Após algum tempo teremos o resultado
+    3. Haverá algum problema e receberemos esse problema
+
+    Não iremos trabalhar tanto criando as promisses, mas sim usando elas.
+
+    Temos 3 status para uma promise:
+
+    1. pendente     - em espera; 
+    2. finalizado   - deu tudo certo; 
+    3. rejeitado    - deu algum erro/problema.
+*/
+
+function pegarTemperatura(){
+    return new Promise(function(resolve, reject){
+        console.log("Pegando temperatura...");
+
+        //simulando o processo de leitura da temperatura
+        setTimeout(function(){
+            //Usaremos a função 'resolve' caso tudo der certo, e a 'reject' caso algo tenha dado errado
+            resolve('40 na sombra');
+        }, 2000);
+    });
+}
+
+// USANDO A PROMISE
+console.log("*Código antes");
+
+let temp = pegarTemperatura();
+
+console.log("*Código durante");
+//callback básico - 'then' permite executar uma função que fará algo uma vez que obtivermos o resultado da promisse
+temp.then(function(resultado){
+    console.log("TEMPERATURA: "+resultado);//utiliza o resultado no código
+});
+//Quando der algum problema - dessa forma temos dois callbacks
+temp.catch(function(error){
+    console.log("Eita, deu erro!");
+});
+
+console.log("*Código depois");
+
+/*  Outra forma válida de declaração
+
+    temp
+        .then(function(resultado){
+        console.log("TEMPERATURA: "+resultado);//utiliza o resultado no código
+        })
+        .temp.catch(function(error){
+        console.log("Eita, deu erro!");
+        });
+*/
